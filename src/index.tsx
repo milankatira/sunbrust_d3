f84773d1d320ff252b8c0c72f3e316ce1f2e3070
milankatira <84834795+milankatira@users.noreply.github.com>
2023-02-20 12:37:35 +0530

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+// Check full tutorial: https://dev.to/andrewchmr/react-d3-sunburst-chart-3cpd
+
+import { render } from "react-dom";
+
+import { SunburstChart } from "./SunburstChart";
+
+const rootElement = document.getElementById("root");
+render(<SunburstChart />, rootElement);
